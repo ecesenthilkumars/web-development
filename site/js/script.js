8b63944267790d9e2db3fe48d63dc8b2abc83d35
Senthil Kumar <ecesenthilkumars@gmail.com>
2020-04-18 19:45:22 +0530

    added bootstrap and jquery

--- site/js/script.js
+++ site/js/script.js
@@ -1,31 +1,29 @@
 let webpage_mainmenu = [
-	{ menu: "Home Page", link: "index.html", class_name: "main_menu_item"},
-	{ menu: "Hardware Booking", link: "hardware.html", class_name: "main_menu_item"},
-	{ menu: "Upcoming Events", link: "events.html", class_name: "main_menu_item"},
-	{ menu: "Visit the Team", link: "team.html", class_name: "main_menu_item"},
+    { menu: "Home Page", link: "index.html", class_name: "main_menu_item col-md-3"},
+    { menu: "Hardware Booking", link: "hardware.html", class_name: "main_menu_item col-md-3"},
+    { menu: "Latest Updates", link: "events.html", class_name: "main_menu_item col-md-3"},
+    { menu: "Meet the Team", link: "team.html", class_name: "main_menu_item col-md-3"}
 ];
 
 let WebpageTitle = "WebSite Title";
 
 function webpage_load()
 {
-	let heading = document.getElementById("page_heading");
-	let newHeading = document.createTextNode(WebpageTitle);
-	heading.appendChild(newHeading);
-	let table = document.getElementById("header_table");
+    let heading = document.getElementById("page_heading");
+    let newHeading = document.createTextNode(WebpageTitle);
+    heading.appendChild(newHeading);
 
-	let menu_row = table.insertRow();
-	for(let element of webpage_mainmenu){
-		let tdata = document.createElement("td");
-		let content = document.createElement("div");
-		let linka = document.createElement("a");
-		let textd = document.createTextNode(element["menu"]);
-		linka.appendChild(textd);
-		linka.href = element["link"];
-		content.appendChild(linka);
-		content.setAttribute("class", element["class_name"]);
 
-		tdata.appendChild(content);
-		menu_row.appendChild(tdata);
-	}
+    let menu_bar = document.getElementById("menu_container");
+    for(let element of webpage_mainmenu){
+            let content = document.createElement("div");
+            let linka = document.createElement("a");
+            let textd = document.createTextNode(element["menu"]);
+            linka.appendChild(textd);
+            linka.href = element["link"];
+            content.appendChild(linka);
+            content.setAttribute("class", element["class_name"]);
+
+            menu_bar.appendChild(content);
+    }
 }
